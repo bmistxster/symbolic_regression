ee0e9eb42d18cf820b43179527e5bc4b5ff5ffda
ajmjd <ajmjd>
2016-04-04 15:57:33 -0700

    Random expr generator

--- tree.py
+++ tree.py
@@ -1,5 +1,6 @@
 import hashlib
 import math
+import random
 
 OPERATOR_ADD = "add"
 OPERATOR_MUL = "mul"
@@ -44,8 +45,11 @@ class BinaryOperator(Node):
             self.rhs = lhs
         md5 = hashlib.md5()
         md5.update(self.id())
+        md5.update("(")
         md5.update(self.lhs.hsh)
+        md5.update(",")
         md5.update(self.rhs.hsh)
+        md5.update(")")
         self.hsh = md5.hexdigest()
 
     def children(self):
@@ -69,7 +73,7 @@ class Mul(BinaryOperator):
         return self.lhs.calculate(values_dict) * self.rhs.calculate(values_dict)
 
     def id(self):
-        return OPERATOR_ADD
+        return OPERATOR_MUL
 
     def __str__(self):
         return "{0} * {1}".format(self.lhs, self.rhs)
@@ -80,7 +84,9 @@ class UnaryOperator(Node):
         self.child = child
         md5 = hashlib.md5()
         md5.update(self.id())
+        md5.update("(")
         md5.update(self.child.hsh)
+        md5.update(")")
         self.hsh = md5.hexdigest()
 
     def children(self):
@@ -151,12 +157,26 @@ class Var(Node):
     def eq(self, other):
         return self.name == other.name
 
+base_exprs = [Const(1), Const(2), Const(-1), Var("x")]
+binary_ops = [Add, Mul]
+unary_ops = [Sin, Sqrt]
+total_exprs = len(base_exprs)
+exprs_count = [total_exprs]
+partial_sums = [total_exprs]
+for level in xrange(10):
+    last_exprs_count = exprs_count[-1]
+    new_count = len(unary_ops) * last_exprs_count
+    new_count += len(binary_ops) * (
+        last_exprs_count * (last_exprs_count + 1) / 2 +
+        last_exprs_count * (total_exprs - last_exprs_count))
+    total_exprs += new_count
+    exprs_count.append(new_count)
+    partial_sums.append(total_exprs)
+
 def generate_expressions(level):
-    exprs = [Const(1), Const(2), Const(-1), Var("x")]
+    exprs = base_exprs
     for expr in exprs:
         yield expr
-    binary_ops = [Add, Mul]
-    unary_ops = [Sin, Sqrt]
     for it in xrange(level):
         new_exprs = set()
         for expr in exprs:
@@ -176,17 +196,50 @@ def generate_expressions(level):
                         new_exprs.add(nexpr)
         exprs = list(new_exprs)
 
-cnt = 0
-for a in generate_expressions(1):
-    print(a)
-    cnt += 1
-    #if cnt % 10000 == 0:
-    #    print(cnt)
-print(cnt)
-a = Var("x")
-b = Var("y")
-c = Add(a, b)
-#print(c)
-#print(c.calculate({"x": 1, "y": 2}))
-#print(isinstance(c, UnaryOperator))
-#print(isinstance(c, BinaryOperator))
+def generate_random_expr(level):
+    if level == 0:
+        return random.choice(base_exprs)
+    last_exprs_count = exprs_count[level - 1]
+    total_exprs = partial_sums[level - 1]
+    unary_op_count = len(unary_ops) * last_exprs_count
+    binary_op_count = len(binary_ops) * (
+        last_exprs_count * (last_exprs_count + 1) / 2 +
+        last_exprs_count * (total_exprs - last_exprs_count))
+    if random.randint(1, unary_op_count + binary_op_count) <= unary_op_count:
+        return random.choice(unary_ops)(generate_random_expr(level - 1))
+    else:
+        op = random.choice(binary_ops)
+        expr_a = generate_random_expr(level - 1)
+        if (random.randint(1, last_exprs_count * (last_exprs_count + 1) / 2 +
+                              last_exprs_count * (total_exprs - last_exprs_count)) <=
+                last_exprs_count * (last_exprs_count + 1) / 2):
+            if (random.randint(1, last_exprs_count * (last_exprs_count + 1) / 2) <=
+                    last_exprs_count):
+                return op(expr_a, expr_a)
+            else:
+                while True:
+                    expr_b = generate_random_expr(level - 1)
+                    if not (expr_a == expr_b):
+                        break
+                return op(expr_a, expr_b)
+        else:
+            val = random.randint(1, partial_sums[level - 2])
+            for other_level in xrange(level - 1):
+                if val <= exprs_count[other_level]:
+                    return op(expr_a, generate_random_expr(other_level))
+                val -= exprs_count[other_level]
+            raise Exception("Should have generated something")
+
+#import collections
+#ccc = collections.defaultdict(int)
+#ttt = 10000000
+#for i in xrange(ttt):
+#    ccc[generate_random_expr(2)] += 1
+
+#avg = ttt / len(ccc)
+#for a, b in ccc.iteritems():
+#    print(b, str(a))
+
+#print(len(ccc), avg)
+
+print(generate_random_expr(4))
